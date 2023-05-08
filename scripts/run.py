dbc1ace8315589e19dcbbc81d8272594c343c251
murad-ali-j <130611518+murad-ali-j@users.noreply.github.com>
2023-05-08 08:33:30 +0100

    Update run.py
    added query statements

--- scripts/run.py
+++ scripts/run.py
@@ -1,26 +1,32 @@
 import os
 import tempfile
+import boto3
 from dataengineeringutils3.s3 import (
     copy_s3_object,
     write_local_file_to_s3,
 )
 
 run = os.getenv("RUN")
-from_path = os.getenv("FROM")
+#from_path = os.getenv("FROM")
 to_path = os.getenv("TO")
-text = os.getenv("TEXT")
-write_outpath = os.getenv("OUTPATH")
+#text = os.getenv("TEXT")
+#write_outpath = os.getenv("OUTPATH")
+db_name = os.getenv("DATABASE")
+tb_name = os.getenv("TABLENAME")
+query_path = os.getenv("QUERYPATH")
 
 if run == "copy":
-    if not from_path or not to_path:
-        raise ValueError(f"Missing FROM and/or TO env vars. Got FROM => {from_path}. TO => {to_path}.")
-    copy_s3_object(from_path, to_path)
+    if not query_path or not to_path:
+        raise ValueError(f"Missing FROM and/or TO env vars. Got FROM => {query_path}. TO => {to_path}.")
+    copy_s3_object(query_path, to_path)
 
-elif run == "write":
-    if not text or not write_outpath:
-        raise ValueError(f"Missing TEXT and/or OUTPATH env vars. Got TEXT => {text}. OUTPATH => {write_outpath}.")
-    with open("tmp.txt", "w") as f:
-        f.write(text)
-    write_local_file_to_s3("tmp.txt", write_outpath, overwrite=True)
+elif run == "query":
+    client = boto3.client("athena")
+    client.start_query_execution(
+        QueryString = 'SELECT count(*) FROM db_name.tb_name;',
+    QueryExecutionContext = {'Database' : db_name},
+    ResultConfiguration={'OutputLocation': query_path,})
+
 else:
     raise ValueError(f"Bad RUN env var. Got {run}. Expected 'copy' or 'write'.")
+
